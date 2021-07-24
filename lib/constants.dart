9fef68b3a4df3164b1c88989f943938a8f7cfaf5
JaeHeee <kjha2142@gmail.com>
2021-07-24 16:03:02 +0900

    feat: Add screenUtil

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ConstColors {
   ConstColors._();
 
-  static const Color mainBlack = Color(0xFF222831);
-  static const Color lightBlack = Color(0xFF393E46);
+  static const Color black = Color(0xFF000000);
   static const Color grey = Color(0xFF8C8C8E);
-  static const Color orange = Color(0xFFD65A31);
-  static const Color yellow = Color(0xFFFFD523);
+  static const Color navy = Color(0xFF0F4C75);
+  static const Color blue = Color(0xFF3282B8);
+  static const Color lightBlue = Color(0xFFBBE1FA);
   static const Color white = Color(0xFFEEEEEE);
 }
 
 class ConstScreen {
   ConstScreen._();
-  static const maxWidth = 1440.0;
-  static const paddingSize = 16.0;
+  static double maxWidth = ScreenUtil().setWidth(1440.0);
+  static double largePadding = ScreenUtil().setWidth(32.0);
+  static double padding = ScreenUtil().setWidth(16.0);
   static const duration = Duration(seconds: 1);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
@@ -12,19 +13,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'JaeHeee.github.io',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: ConstColors.orange,
-        backgroundColor: ConstColors.mainBlack,
-        canvasColor: ConstColors.mainBlack,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: ConstColors.white)
-            .copyWith(
-              caption: const TextStyle(color: ConstColors.white),
-            ),
+    return ScreenUtilInit(
+      builder: () => MaterialApp(
+        title: 'JaeHeee.github.io',
+        theme: ThemeData().copyWith(
+          primaryColor: ConstColors.black,
+          scaffoldBackgroundColor: ConstColors.black,
+          textTheme: GoogleFonts.antonTextTheme(Theme.of(context).textTheme)
+              .apply(
+                bodyColor: ConstColors.white,
+              )
+              .copyWith(
+                headline1: const TextStyle(color: ConstColors.white),
+                headline2: const TextStyle(color: ConstColors.white),
+                headline3: const TextStyle(color: ConstColors.white),
+                headline4: const TextStyle(color: ConstColors.white),
+                headline5: const TextStyle(color: ConstColors.white),
+                headline6: const TextStyle(color: ConstColors.white),
+                subtitle1: const TextStyle(color: ConstColors.white),
+                subtitle2: const TextStyle(color: ConstColors.white),
+                bodyText1: const TextStyle(color: ConstColors.white),
+                bodyText2: const TextStyle(color: ConstColors.white),
+                caption: const TextStyle(color: ConstColors.white),
+                button: const TextStyle(color: ConstColors.white),
+                overline: const TextStyle(color: ConstColors.white),
+              ),
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
